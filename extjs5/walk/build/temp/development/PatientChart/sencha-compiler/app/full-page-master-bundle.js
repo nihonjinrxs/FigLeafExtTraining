a1b54a00652cf4459c3169a5825be7b7000f5610
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-16 14:51:52 -0400

    Fig Leaf Training Walkthrough 2-3: Generating an App.  App generated.

--- extjs5/walk/build/temp/development/PatientChart/sencha-compiler/app/full-page-master-bundle.js
+++ extjs5/walk/build/temp/development/PatientChart/sencha-compiler/app/full-page-master-bundle.js
@@ -1,2 +1,2 @@
 // @tag full-page
-// @require /Library/WebServer/Documents/figleaftraining/extjs5/walk/walk7-1/app.js
+// @require /Users/ryan/Dropbox/courses/extjs-figleaf/extjs5bootcamp/figleaftraining/extjs5/walk/walk2-3/app.js
