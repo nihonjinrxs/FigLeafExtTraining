5874040f8513745aac38ad6b1adeaa8d12daccc6
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-20 00:18:13 -0400

    Walkthrough 5-1: Configured patientinfo.Chooser view.

--- extjs5/walk/walk5-1/app/view/patientinfo/Chooser.js
+++ extjs5/walk/walk5-1/app/view/patientinfo/Chooser.js
@@ -1,10 +1,16 @@
 
 Ext.define('PatientChart.view.patientinfo.Chooser',{
     extend: 'Ext.window.Window',
+    alias: 'widget.patientsearchwindow',
 
     requires: [
         'PatientChart.view.patientinfo.ChooserController'
     ],
 
-    controller: 'patientinfo-chooser'
+    controller: 'patientinfo-chooser',
+
+    width: 700,
+    height: 400,
+    layout: 'fit',
+    title: 'Select a Patient'
 });
