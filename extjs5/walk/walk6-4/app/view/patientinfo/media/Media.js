2edd461b83eed3640dc8634dccefaa9c172db485
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-21 18:09:16 -0400

    Walkthrough 6-4: Added sort and filter options to the PatientInfo media assets view and animated changes.

--- extjs5/walk/walk6-4/app/view/patientinfo/media/Media.js
+++ extjs5/walk/walk6-4/app/view/patientinfo/media/Media.js
@@ -1,11 +1,21 @@
 Ext.define("PatientChart.view.patientinfo.media.Media", {
 	extend: "Ext.panel.Panel",
 	requires: [
-		'PatientChart.view.patientinfo.media.MediaController'
+		'PatientChart.view.patientinfo.media.MediaController',
+		'Ext.ux.DataView.Animated'
 	],
 	alias: 'widget.patientmedia',
 	controller: "patientinfo-media-media",
 	layout: 'fit',
+	plugins: [
+		Ext.create('Ext.ux.DataView.Animated', {
+			duration: 550,
+			idProperty: 'id'
+		})
+	],
+	tools: [
+		{ xtype: 'tool', type: 'gear', callback: 'onShowMenu' }
+	],
 	items: [{
 		xtype: 'dataview',
 		scrollable: true,
