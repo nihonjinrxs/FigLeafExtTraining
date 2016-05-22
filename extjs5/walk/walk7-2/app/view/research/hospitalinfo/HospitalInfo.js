f9752363026417b7fefbcfa70c2732a612313254
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-21 20:40:06 -0400

    Walkthrough 7-2: Added TreeGrid to HospitalInfo view and wired to store in ViewModel.

--- extjs5/walk/walk7-2/app/view/research/hospitalinfo/HospitalInfo.js
+++ extjs5/walk/walk7-2/app/view/research/hospitalinfo/HospitalInfo.js
@@ -1,6 +1,7 @@
 
 Ext.define('PatientChart.view.research.hospitalinfo.HospitalInfo',{
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.window.Window',
+    alias: 'widget.hospitalinfotreegrid',
 
     requires: [
         'PatientChart.view.research.hospitalinfo.HospitalInfoController',
@@ -12,5 +13,67 @@ Ext.define('PatientChart.view.research.hospitalinfo.HospitalInfo',{
         type: 'research-hospitalinfo-hospitalinfo'
     },
 
-    html: 'Hello, World!!'
+    layout: 'fit',
+    constrain: true,
+    autoShow: true,
+    maximizable: true,
+    height: 475,
+    width: 900,
+    title: 'Hospital Treatment Costs',
+
+    items: [
+        {
+            xtype: 'treepanel',
+            rootVisible: false,
+            useArrows: true,
+            showHeader: false,
+            bind: {
+                store: '{HospitalInfo}'
+            },
+            columns: [
+                {
+                    xtype: 'treecolumn',
+                    dataIndex: 'text',
+                    flex: 1,
+                    text: 'Hospital'
+                },
+                {
+                    xtype: 'numbercolumn',
+                    dataIndex: 'treatmentcount',
+                    text: '# Treatments',
+                    format: '0',
+                    align: 'right'
+                },
+                {
+                    xtype: 'numbercolumn',
+                    dataIndex: 'totaldischarges',
+                    text: 'Discharges',
+                    format: '0,0',
+                    align: 'right'
+                },
+                {
+                    xtype: 'numbercolumn',
+                    dataIndex: 'averagetotalpayments',
+                    text: 'Avg Payments',
+                    format: '0,0',
+                    align: 'right'
+                },
+                {
+                    xtype: 'numbercolumn',
+                    dataIndex: 'averagemedicarepayments',
+                    text: 'Avg Medicare<br/>Payments',
+                    format: '0,0',
+                    align: 'right'
+                },
+                {
+                    xtype: 'numbercolumn',
+                    dataIndex: 'averagecoveredcharges',
+                    text: 'Avg Covered<br/>Charges',
+                    format: '0,0',
+                    align: 'right'
+                }
+            ]
+        }
+    ]
+
 });
