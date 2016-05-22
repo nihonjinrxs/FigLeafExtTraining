8c2f619515faf87ee8cd0711fc2cbd5b1efc9d59
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-21 20:56:36 -0400

    Walkthrough 7-3: Defined showDetails method in HospitalInfo view to log TreeGrid node type to console when double-clicked.

--- extjs5/walk/walk7-3/app/view/research/hospitalinfo/HospitalInfoController.js
+++ extjs5/walk/walk7-3/app/view/research/hospitalinfo/HospitalInfoController.js
@@ -1,7 +1,9 @@
 Ext.define('PatientChart.view.research.hospitalinfo.HospitalInfoController', {
 	extend: 'Ext.app.ViewController',
-	alias: 'controller.research-hospitalinfo-hospitalinfo'
+	alias: 'controller.research-hospitalinfo-hospitalinfo',
 
-	
+	showDetails: function(grid, record, item, index, event, eventOptions) {
+		console.log(record, record.$className);
+	}
 
 });
