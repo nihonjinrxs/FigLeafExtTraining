b0cf80b4ba4517cc529810a521e91ccfa0e25e44
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-24 15:00:03 -0400

    Walkthrough 8-2: Added HeightWeight line chart display to PatientInfo Graphs view.

--- extjs5/walk/walk8-2/app/view/patientinfo/graphs/HeightWeight.js
+++ extjs5/walk/walk8-2/app/view/patientinfo/graphs/HeightWeight.js
@@ -1,5 +1,63 @@
 Ext.define("PatientChart.view.patientinfo.graphs.HeightWeight", {
-	extend: "Ext.panel.Panel",
+	extend: "Ext.chart.CartesianChart",
 	alias: 'widget.patientheightweight',
-	header: false
+
+	requires: [
+		'Ext.chart.axis.Category',
+		'Ext.chart.axis.Numeric',
+		'Ext.chart.series.Line',
+		'Ext.chart.interactions.Crosshair',
+		'Ext.chart.interactions.ItemHighlight'
+	],
+
+	header: false,
+
+	bind: {
+		store: '{PatientDailyStats}'
+	},
+
+	axes: [
+		{
+			type: 'category',
+			fields: [ 'datelabel' ],
+			title: 'Date',
+			position: 'bottom',
+			label: { rotate: { degrees: -45 } }
+		},
+		{
+			type: 'numeric',
+			fields: [ 'height' ],
+			position: 'left',
+			title: 'Height'
+		},
+		{
+			type: 'numeric',
+			fields: [ 'weight' ],
+			position: 'right',
+			title: 'Weight',
+			grid: {
+				odd: {
+					opacity: 1,
+					fill: '#ddd',
+					stroke: '#bbb',
+					linewidth: 1
+				}
+			}
+		}
+	],
+
+	series: [
+		{
+			type: 'line',
+			xField: 'datelabel',
+			yField: [ 'height' ],
+			smooth: true
+		},
+		{
+			type: 'line',
+			xField: 'datelabel',
+			yField: [ 'weight' ],
+			smooth: true
+		}
+	]
 });
