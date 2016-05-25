037510d1defea60ceb389a056acf9531ac7973b7
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-25 18:28:11 -0400

    Walkthrough 10-2: Added PatientInfo Procedures editable grid, and enabled model-based validations on grid fields.

--- extjs5/walk/walk10-2/app/view/patientinfo/procedures/Grid.js
+++ extjs5/walk/walk10-2/app/view/patientinfo/procedures/Grid.js
@@ -1,6 +1,71 @@
 Ext.define('PatientChart.view.patientinfo.procedures.Grid', {
-	extend: 'Ext.panel.Panel',
+	extend: 'Ext.grid.Panel',
 	alias: 'widget.patientproceduregrid',
-	title: 'Visits & Procedures'
+	
+	requires: [
+		'PatientChart.view.patientinfo.procedures.ProceduresCombo'
+	],
+
+	bind: {
+		store: '{PatientVisits}',
+		selection: '{selectedProcedure}'
+	},
+
+	plugins: {
+		ptype: 'rowediting',
+		clicksToEdit: 2
+	},
+
+	title: 'Visits & Procedures',
+
+	columns: [
+		{
+			xtype: 'datecolumn',
+			dataIndex: 'date',
+			text: 'Date',
+			width: 120,
+			editor: {
+				xtype: 'datefield',
+				bind: '{selectedProcedure.date}',
+				modelValidation: true
+			}
+		},
+		{
+			xtype: 'gridcolumn',
+			dataIndex: 'procedureId',
+			text: 'Proc ID',
+			flex: 1,
+			renderer: function(value, metadata, rec) {
+				return rec.get('code') + ' - ' + rec.get('text')
+			},
+			editor: {
+				xtype: 'procedurescombo',
+				bind: '{selectedProcedure.procedureId}',
+				modelValidation: true
+			}
+		},
+		{
+			xtype: 'gridcolumn',
+			dataIndex: 'description',
+			text: 'Description',
+			flex: 1,
+			editor: {
+				xtype: 'textfield',
+				bind: '{selectedProcedure.description}',
+				modelValidation: true
+			}
+		},
+		{
+			xtype: 'numbercolumn',
+			dataIndex: 'fee',
+			text: 'Fee',
+			align: 'right',
+			editor: {
+				xtype: 'numberfield',
+				bind: '{selectedProcedure.fee}',
+				modelValidation: true
+			}
+		}
+	]
 
 });
