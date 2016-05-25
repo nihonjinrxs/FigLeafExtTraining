803a91728b0595c03da345e92e141c259dc60178
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-25 14:50:26 -0400

    Walkthrough 10-1: Added basic form with two column layout to PatientInfo Procedures sub-view.

--- extjs5/walk/walk10-1/app/view/patientinfo/procedures/Form.js
+++ extjs5/walk/walk10-1/app/view/patientinfo/procedures/Form.js
@@ -1,6 +1,44 @@
 
 Ext.define("PatientChart.view.patientinfo.procedures.Form",{
-    extend: "Ext.panel.Panel",
-    alias: 'widget.patientprocedureform'
+    extend: "Ext.form.Panel",
+    alias: 'widget.patientprocedureform',
+
+    requires: [
+    	'Ext.layout.container.Column',
+    	'Ext.form.field.Date',
+    	'Ext.form.field.Number',
+    	'Ext.form.field.ComboBox',
+    	'Ext.ux.form.TinyMCETextArea'
+    ],
+
+    bodyPadding: 10,
+
+    defaults: {
+    	anchor: '100%'
+    },
+
+    items: [
+    	{
+    		xtype: 'container',
+    		layout: 'column',
+    		defaults: {
+    			columnWidth: 0.5,
+    			margin: '0 5 5 0'
+    		},
+    		items: [
+    			{
+    				xtype: 'datefield',
+    				name: 'date',
+    				fieldLabel: 'Date'
+    			},
+    			{
+    				xtype: 'numberfield',
+    				name: 'fee',
+    				fieldLabel: 'Fee',
+    				minValue: 0
+    			}
+    		]
+    	}
+    ]
   
 });
