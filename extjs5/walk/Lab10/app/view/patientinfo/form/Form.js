8503f82a16ac5822468a963ab26ba8ac3c0a8714
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-05-26 01:28:55 -0400

    Challenge Lab 10: Implemented PatientInfo main form.

--- extjs5/walk/Lab10/app/view/patientinfo/form/Form.js
+++ extjs5/walk/Lab10/app/view/patientinfo/form/Form.js
@@ -1,6 +1,6 @@
 
 Ext.define("PatientChart.view.patientinfo.form.Form",{
-    extend: "Ext.panel.Panel",
+    extend: "Ext.form.Panel",
     alias: 'widget.patientform',
     requires: [
     	'PatientChart.view.patientinfo.form.FormController',
@@ -9,6 +9,208 @@ Ext.define("PatientChart.view.patientinfo.form.Form",{
     controller: "patientinfo-form-form",
     viewModel: {
 		type: "formmodel"
-	}
+	},
+
+    layout: 'auto',
+    bodyPadding: 5,
+    title: 'Patient Information',
+    modelValidation: true,
+
+    listeners: {
+        'beforerender': 'onFormBeforeRender'
+    },
+
+    tools: [{
+        xtype: 'tool',
+        type: 'save',
+        formBind: true,
+        callback: 'saveForm'
+    }],
+
+    items: [
+        {
+            xtype: 'container',
+            layout: {
+                type: 'hbox',
+                align: 'stretch'
+            },
+            items: [
+                {
+                    xtype: 'component',
+                    border: 1,
+                    height: 164,
+                    bind: {
+                        style: {
+                            border: '1px solid black',
+                            'border-radius': '5px',
+                            'background-image': 'url({profileImage})',
+                            'background-repeat': 'no-repeat',
+                            'background-position': 'center'
+                        }
+                    },
+                    width: 173
+                },
+                {
+                    xtype: 'container',
+                    flex: 1,
+                    margin: '0 5 0 5',
+                    defaults: {
+                        anchor: '100%'
+                    },
+                    layout: 'anchor',
+                    items: [
+                        {
+                            xtype: 'hiddenfield',
+                            name: 'patientid',
+                            bind: '{patient.id}'
+                        },
+                        {
+                            xtype: 'textfield',
+                            name: 'firstname',
+                            fieldLabel: 'First Name',
+                            bind: '{patient.firstname}',
+                            allowBlank: false
+                        },
+                        {
+                            xtype: 'textfield',
+                            name: 'lastname',
+                            fieldLabel: 'Last Name',
+                            bind: '{patient.lastname}',
+                            allowBlank: false
+                        },
+                        {
+                            xtype: 'textfield',
+                            name: 'address',
+                            fieldLabel: 'Address',
+                            bind: '{patient.address}'
+                        },
+                        {
+                            xtype: 'fieldcontainer',
+                            layout: 'hbox',
+                            fieldLabel: 'City, State, Z',
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    name: 'city',
+                                    fieldLabel: '',
+                                    bind: '{patient.city}',
+                                    flex: 1,
+                                    margin: '0 5 0 0'
+                                },
+                                {
+                                    xtype: 'combobox',
+                                    name: 'state',
+                                    bind: '{patient.state}',
+                                    hideLabel: true,
+                                    width: 80,
+                                    margin: '0 5 0 0',
+                                    displayField: 'abbreviation',
+                                    valueField: 'abbreviation',
+                                    bind: {
+                                        store: '{States}',
+                                        value: '{patient.state}'
+                                    },
+                                    queryMode: 'local',
+                                    forceSelection: true
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    name: 'zipcode',
+                                    bind: '{patient.zipcode}',
+                                    hideLabel: true,
+                                    width: 80
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'textfield',
+                            vtype: 'email',
+                            name: 'email',
+                            fieldLabel: 'Email',
+                            bind: '{patient.email}'
+                        },
+                        {
+                            xtype: 'datefield',
+                            name: 'dob',
+                            fieldLabel: 'Date of Birth',
+                            bind: '{patient.dob}'
+                        },
+                        {
+                            xtype: 'radiogroup',
+                            fieldLabel: 'Gender',
+                            layout: 'hbox',
+                            allowBlank: false,
+                            bind: { value: 'patient.gender' },
+                            items: [
+                                {
+                                    xtype: 'radiofield',
+                                    name: 'gender',
+                                    boxLabel: 'Male',
+                                    inputValue: 'Male',
+                                    margin: '0 10 0 0'
+                                },
+                                {
+                                    xtype: 'radiofield',
+                                    name: 'gender',
+                                    boxLabel: 'Female',
+                                    inputValue: 'Female'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            xtype: 'container',
+            margin: '10 5 0 0',
+            layout: 'anchor',
+            defaults: {
+                anchor: '100%'
+            },
+            items: [
+                {
+                    xtype: 'filefield',
+                    name: 'profilePhoto',
+                    fieldLabel: 'Upload Image'
+                },
+                {
+                    xtype: 'tagfield',
+                    name: 'allergies',
+                    fieldLabel: 'Allergies',
+                    store: 'Allergies',
+                    bind: { value: '{patient.allergies}' }
+                },
+                {
+                    xtype: 'fieldset',
+                    title: 'Pre-Existing Conditions',
+                    anchor: '100% -250',
+                    items: [
+                        {
+                            xtype: 'checkboxgroup',
+                            bind: { value: '{patient.preexistingconditions}' },
+                            reference: 'preexistingconditions',
+                            hideLabel: true,
+                            items: [
+                                {
+                                    xtype: 'checkboxfield',
+                                    fieldLabel: 'Label',
+                                    boxLabel: 'Box Label'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    xtype: 'tinymce',
+                    name: 'notes',
+                    fieldLabel: 'Notes',
+                    bind: '{patient.notes}',
+                    labelAlign: 'top',
+                    height: 300
+                }
+            ]
+        }
+    ]
   
 });
--- extjs5/walk/Lab10/app/view/patientinfo/form/FormController.js
+++ extjs5/walk/Lab10/app/view/patientinfo/form/FormController.js
@@ -1,5 +1,68 @@
 Ext.define('PatientChart.view.patientinfo.form.FormController', {
     extend: 'Ext.app.ViewController',
-    alias: 'controller.patientinfo-form-form'
-    
+    alias: 'controller.patientinfo-form-form',
+
+	onFormBeforeRender: function(component, eventOptions) {
+		var pexc = this.lookupReference('preexistingconditions');
+		var s = Ext.getStore('PreExistingConditions');
+		if (s.getCount() == 0) {
+			Ext.defer(this.onFormBeforeRender, 100, this, [component, eventOptions]);
+		} else {
+			pexc.removeAll();
+			var checkboxes = [];
+			for (var i = 0; i < s.getCount(); i++) {
+				checkboxes.push({
+					xtype: 'checkbox',
+					name: 'preexistingcondition',
+					boxLabel: s.getAt(i).get('text'),
+					inputValue: s.getAt(i).get('id')
+				});
+			}
+			pexc.add(checkboxes);
+		}
+	},
+
+	saveForm: function(form, tool) {
+		var record = this.getViewModel().get('patient');
+
+		// save metatdata through proxy
+		record.save({
+			success: function(rec, operation) {
+				Ext.toast('Record Updated', 'Server Transaction', 't');
+				if (operation.action == 'create') {
+					var pk = Ext.decode(operation.getResponse().responseText).id;
+					record.set('id', pk);
+				}
+				this.uploadPhoto(form);
+			},
+			failure: function(rec, operation) {
+				Ext.Msg.alert("Failure", "Operation failed. Check your browser console");
+				console.log(arguments);
+			},
+			scope: this
+		});
+
+	},
+
+	uploadPhoto: function(form) {
+		form.submit({
+			url: 'http://webapps.figleaf.com/webservices/savepatient.cfm',
+			success: function(form, action) {
+				Ext.Msg.alert("Success!");
+			},
+			failure: function(form, action) {
+				switch (action.failureType) {
+					case Ext.form.action.Action.CLIENT_INVALID:
+						Ext.Msg.alert('Failure', 'Form fields may not be submitted with invalid values');
+						break;
+					case Ext.form.action.Action.CONNECT_FAILURE:
+						Ext.Msg.alert('Failure', 'Ajax communication failed');
+						break;
+					case Ext.form.action.Action.SERVER_INVALID:
+						Ext.Msg.alert('Failure', action.result.msg);
+				}
+			}
+		});
+	}
+
 });
